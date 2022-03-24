96c16358de97d1d59f8277797c2b960209b8facd
IgorSilvaZZ <igor06silva12@gmail.com>
2022-03-24 10:15:44 -0300

    Finish tests sportsRegister

--- src/__tests__/sport/SportRegisterUseCase.test.js
+++ src/__tests__/sport/SportRegisterUseCase.test.js
@@ -4,14 +4,14 @@ const { createFakerUser } = require("../../utils/createFakerUser");
 const baseUrl = "https://api-stg.sportidia.com/graphql";
 
 describe("Registro de Esporte Casos de sucesso!", () => {
-  /*  it("#3.0 => O sistema cadastra o esport e me retorna um json da resposta", async () => {
-    const { suffix } = createFakerUser();
+  it("#3.0 => O sistema cadastra o esporte e me retorna um json da resposta", async () => {
+    const { firstName } = createFakerUser();
 
     const query = `
             mutation fieldTestName{
                 sportRegister(
                 name: "Basquete"
-                slug: "${suffix}"
+                slug: "${firstName}"
                 ){
                 name
                 }
@@ -25,15 +25,16 @@ describe("Registro de Esporte Casos de sucesso!", () => {
     expect(body.data).toHaveProperty("sportRegister");
     expect(body.data.sportRegister).toHaveProperty("name");
     expect(body.data.sportRegister.name).toBe("Basquete");
-  }); */
-  /* it("# 3.1 => insiro um valor vazio no campo name", async () => {
-    const { suffix } = createFakerUser();
+  });
+
+  it("# 3.1 => insiro um valor vazio no campo name", async () => {
+    const { firstName } = createFakerUser();
 
     const query = `
         mutation fieldTestName{
             sportRegister(
             name: ""
-            slug: "${suffix}"
+            slug: "${firstName}"
             ){
                 name
             }
@@ -49,26 +50,47 @@ describe("Registro de Esporte Casos de sucesso!", () => {
     const [error] = body.errors;
 
     expect(error.message).toBe("Bad Request Exception");
-  }); */
-  /* test.each([
+  });
+
+  test.each([
     {
       campo: "name",
       name: "@@@@@@@",
-      suffix: createFakerUser().suffix,
+      suffix: createFakerUser().firstName,
+      image_url: "https://testeimagem.com/imagem.png",
+      pin_image_url: "testeCampo",
     },
     {
       campo: "slug",
       name: "Basquete",
-      suffix: `${createFakerUser().suffix}@@@@@@@`,
+      suffix: `${createFakerUser().firstName}@@@@@@@`,
+      image_url: "https://testeimagem.com/imagem.png",
+      pin_image_url: "testeCampo",
+    },
+    {
+      campo: "image_url",
+      name: "Basquete",
+      suffix: `${createFakerUser().firstName}`,
+      image_url: "@@@@@@@",
+      pin_image_url: "testeCampo",
+    },
+    {
+      campo: "pin_image_url",
+      name: "Basquete",
+      suffix: `${createFakerUser().firstName}`,
+      image_url: "@@@@@@@",
+      pin_image_url: "@@@@@@@",
     },
   ])(
     "Testes com campo $campo com valor de caracteres especiais",
-    async ({ campo, name, suffix }) => {
+    async ({ campo, name, suffix, image_url, pin_image_url }) => {
       const query = `
         mutation fieldTestName{
             sportRegister(
             name: "${name}"
             slug: "${suffix}"
+            image_url: "${image_url}"
+            pin_image_url: "${pin_image_url}"
             ){
               ${campo}
             }
@@ -81,8 +103,9 @@ describe("Registro de Esporte Casos de sucesso!", () => {
 
       expect(body.data).toHaveProperty("sportRegister");
     }
-  ); */
-  /* test.each([["123456"], ["true"], ["false"], ["null"]])(
+  );
+
+  test.each([["123456"], ["true"], ["false"], ["null"]])(
     "Espero que o campo name não sejam cadastrados com valor %s ",
     async (valor) => {
       await expect(async () => {
@@ -102,8 +125,9 @@ describe("Registro de Esporte Casos de sucesso!", () => {
         await requestForApiGraphQL(baseUrl, query);
       }).rejects.toThrowError();
     }
-  ); */
-  /* test.each([["123456"], ["true"], ["false"], ["null"]])(
+  );
+
+  test.each([["123456"], ["true"], ["false"], ["null"]])(
     "Espero que o campo slug não sejam cadastrados com valor %s ",
     async (valor) => {
       await expect(async () => {
@@ -121,8 +145,9 @@ describe("Registro de Esporte Casos de sucesso!", () => {
         await requestForApiGraphQL(baseUrl, query);
       }).rejects.toThrowError();
     }
-  ); */
-  /* it("#3.15 => Insiro um valor existente no campo slug", async () => {
+  );
+
+  it("#3.15 => Insiro um valor existente no campo slug", async () => {
     const { suffix } = createFakerUser();
 
     const query = `
@@ -144,8 +169,9 @@ describe("Registro de Esporte Casos de sucesso!", () => {
 
     expect(body).toHaveProperty("errors");
     expect(body.errors[0].message).toBe("SLUGALREADYEXISTS");
-  }); */
-  /* it("# 3.17 => insiro um valor string no campo image_url", async () => {
+  });
+
+  it("# 3.17 => insiro um valor string no campo image_url", async () => {
     const { companySuffix } = createFakerUser();
 
     const query = `
@@ -165,16 +191,9 @@ describe("Registro de Esporte Casos de sucesso!", () => {
     const { body } = response;
 
     expect(body).toHaveProperty("data");
-    expect(body.data).toEqual(
-      expect.objectContaining({
-        sportRegister: {
-          image_url: "https://testeimagem.com/imagem.png",
-        },
-      })
-    );
-  }); */
+  });
 
-  test.each([["123456"], [true], [false], [null]])(
+  test.each([[""], ["123456"], [true], [false], [null]])(
     "Espero que o campo image_url não sejam cadastrados com valor %s ",
     async (valor) => {
       await expect(async () => {
@@ -197,25 +216,51 @@ describe("Registro de Esporte Casos de sucesso!", () => {
     }
   );
 
-  /* it("# 3.19 insiro um valor númerico no campo image_url => ", async () => {
-    await expect(async () => {
-      const { companySuffix } = createFakerUser();
+  it("# 3.25 insiro um valor string no campo image_url => ", async () => {
+    const { firstName } = createFakerUser();
 
-      const query = `
-        mutation fieldTestImageUrl{
-            sportRegister(
-              name: "testeImageUrl1"
-              slug: "${companySuffix}",
-              image_url: 123456
-            ){
-              image_url
+    const query = `
+      mutation fieldTestPinImage{
+          sportRegister(
+            name: "testeImageUrl1"
+            slug: "${firstName}",
+            image_url: "https://testepinimage.com/pinimagem.png"
+            pin_image_url: "testeCampo"
+          ){
+            pin_image_url
+          }
+      }
+    `;
+
+    const response = await requestForApiGraphQL(baseUrl, query);
+
+    const { body } = response;
+
+    expect(body.data).toHaveProperty("sportRegister");
+    expect(body.data.sportRegister.pin_image_url).toBe("testeCampo");
+  });
+
+  test.each([[""], ["123456"], [true], [false], [null]])(
+    "Espero que o campo pin_image_url não sejam cadastrados com valor %s ",
+    async (valor) => {
+      await expect(async () => {
+        const { firstName } = createFakerUser();
+
+        const query = `
+            mutation fieldTestName{
+                sportRegister(
+                    name: "Basquete"
+                    slug: "${firstName}",
+                    image_url: "https://testepinimage.com/pinimagem.png"
+                    pin_image_url: ${valor}
+                ){
+                  pin_image_url
+                }
             }
-        }
-      `;
+        `;
 
-      await requestForApiGraphQL(baseUrl, query);
-    }).rejects.toEqual(
-      new Error("String cannot represent a non string value: 123456")
-    );
-  }); */
+        await requestForApiGraphQL(baseUrl, query);
+      }).rejects.toThrowError();
+    }
+  );
 });
